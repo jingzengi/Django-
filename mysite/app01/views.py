d407cd40c1edcd5083a0f479d5d26994663eb7a2
zjing306 <qingyi1314@live.cn>
2022-09-05 14:05:24 +0800

    11

--- mysite/app01/views.py
+++ mysite/app01/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    import datetime
+    now = datetime.datetime.now()
+    ctime = now.strftime("%Y-%m-%d %X")
+    # return  render(request,"index.html", {"ctime": ctime})
+    return HttpResponse('index page...')
